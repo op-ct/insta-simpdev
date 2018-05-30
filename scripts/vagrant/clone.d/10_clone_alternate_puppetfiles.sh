a2aec88bea8c986bd1ff2bae667e14e9f1bc2be5
Chris Tessmer <chris.tessmer@onyxpoint.com>
2018-05-30 19:49:29 +0000

    Improve output

--- scripts/vagrant/clone.d/10_clone_alternate_puppetfiles.sh
+++ scripts/vagrant/clone.d/10_clone_alternate_puppetfiles.sh
@@ -9,6 +9,8 @@
 #   SIMP_BUILDER_puppetfile_ref (optional, defaults to `master`)
 #
 
+me=$(basename $0)
+
 # fetch Puppetfile.* repo @ ref
 if [ -n "${SIMP_BUILDER_puppetfile_repo}" ]; then
   tmp_dir=$(mktemp -d -t build_iso.sh.XXXXXXXXXX)
@@ -16,7 +18,6 @@ if [ -n "${SIMP_BUILDER_puppetfile_repo}" ]; then
 
   repo_url="${SIMP_BUILDER_puppetfile_repo}"
   repo_ref="${SIMP_BUILDER_puppetfile_ref:-master}"
-  me=$(basename $0)
 
   echo "  [${me}]: -- cloning repo with new Puppetfile.*: ${repo_url} -b ${repo_ref}"
   git clone --depth 1 "${repo_url}" -b "${repo_ref}" "${tmp_dir}"
@@ -24,4 +25,8 @@ if [ -n "${SIMP_BUILDER_puppetfile_repo}" ]; then
   popd &> /dev/null
   mv "${tmp_dir}"/Puppetfile.* ./
   [[ -d "${tmp_dir}" ]] && rm -rf "${tmp_dir}"
+else
+  echo "  [${me}]: -- SIMP_BUILDER_puppetfile_repo is not set"
+  echo "  [${me}]: -- SIMP_BUILDER_puppetfile_ref is not set"
+  echo "  [${me}]: -- "'will not attempt to check out alternate `Puppetfile.*` files'
 fi
