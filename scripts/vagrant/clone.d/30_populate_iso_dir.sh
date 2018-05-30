b0ed2ee43c958e60b164eb91bb29c996a386acd8
Chris Tessmer <chris.tessmer@onyxpoint.com>
2018-05-30 19:49:05 +0000

    Fix variable name

--- scripts/vagrant/clone.d/30_populate_iso_dir.sh
+++ scripts/vagrant/clone.d/30_populate_iso_dir.sh
@@ -1,4 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
+#
+# 1. Create the 'simp-core/ISO' directory
+# 2. Link or copy *.iso files under `$SIMP_BUILDER_iso_dir` into 'simp-core/ISO'
+#
+# Notes:
+#
+#   * files on the same mount as 'simp-core/ISO' will be hard-linked
+#   * files that are (somehow) on different mounts will be rsynced
 #
 
 declare -a ISO_FILES=()
@@ -6,8 +14,8 @@ declare -a ISO_FILES=()
 if [[ $? -eq 0 ]]; then
   while read -d $'\0' file; do
     declare -a -g ISO_FILES
-     echo "Found ISO file: '${file}'"
-     ISO_FILES+=( "${file}" )
+    echo "Found ISO file: '${file}'"
+    ISO_FILES+=( "${file}" )
   done < <( find "${SIMP_BUILDER_iso_dir:-/vagrant/downloads/isos}" -type f \
             -iname \*.iso -print0 )
 else
@@ -21,13 +29,13 @@ cd simp-core
 
 simp_core_mount=$(findmnt -T . -P)
 mkdir -p ISO
-for file in "${ISO_FILES[@]}"; do
-  filename="$(basename "${file}")"
-  file_mount=$(findmnt -T "${file}" -P)
+for src_file in "${ISO_FILES[@]}"; do
+  file_name="$(basename "${src_file}")"
+  file_mount=$(findmnt -T "${src_file}" -P)
 
   if [ "${simp_core_mount}" = "${file_mount}" ]; then
-    ln -f "${f}" ISO/"${filename}"
+    ln -f "${src_file}" ISO/"${file_name}"
   else
-    rsync -avz --progress "${file}" ISO/"${filename}"
+    rsync -avz --progress "${src_file}" ISO/"${file_name}"
   fi
 done
